04c6207336e3d7cafbac2ea36e9d2babae661e73
fatimashahzad00 <fatimashahzad062@gmail.com>
2023-02-04 20:11:25 +0500

    data-types-examples

---
+++ Project1/script.js
@@ -0,0 +1,24 @@
+                    //   08 - 10 - 2022 Class work
+
+
+// // This is the main JS of Project 1
+// const userName = prompt("Hi, My name is Fatima. what's your name?")
+// console.log(userName)
+
+// Bad Code
+var x = "Imran";
+
+// Good Code
+
+console.log("before creating variable", teacherName)
+
+var teacherName = "Imran";
+
+console.log("after creating variable", teacherName)
+
+var subject = "Javascript";
+
+// If Sir imran went to america and want to change name
+var teacherName = "Alexendar"
+
+console.log("after reassigning variable", teacherName)
---
+++ Project1/script2.js
@@ -0,0 +1,20 @@
+let teacherName2 = "Alexender";
+console.log(typeof teacherName2)
+
+let teacherAge = 18;
+console.log(typeof teacherAge)
+
+let changedName = true;
+console.log(typeof changedName)
+
+let teacherClass = undefined;
+console.log(typeof teacherClass)
+
+let teacherFavouriteStudent = null;
+console.log(typeof teacherFavouriteStudent)
+
+
+
+let var1 = "Imran";
+var1 = "Alexendar";
+var1 = 18;
